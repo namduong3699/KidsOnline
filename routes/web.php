203de4a7d715a9abeb2957c0a70c5fd672894704
huyenchau <nkhchau@gmail.com>
2019-09-15 22:32:40 +0700

    trang 15/9

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,88 @@
 |
 */
 
+Route::get('giao-vien', function () {
+    return view('user-teacher.home');
+
+});
+
+Route::get('diem-danh', function () {
+    return view('user-teacher.diem-danh');
+});
+
+//phụ huynh
+Route::get('phu-huynh', function () {
+    return view('user-student.main');
+});
+
+Route::get('xin-nghi', function () {
+    return view('user-student.xin-nghi');
+});
+
+Route::get('loi-nhan', function () {
+    return view('user-student.loi-nhan');
+});
+
+Route::get('profileGU', function () {
+    return view('user-student.profileGU');
+});
+
+
+Route::get('hoat-dong-cua-be', function () {
+    return view('user-student.hoat-dong-cua-be');
+});
+
+Route::get('anh-cua-be', function () {
+    return view('user-student.anh-cua-be');
+});
+
+Route::get('dan-thuoc', function () {
+    return view('user-student.dan-thuoc');
+});
+
+Route::get('bai-viet', function () {
+    return view('user-student.bai-viet');
+});
+
+
+// Route::get('1', function () {
+//     return view('user-guest.thuguest');
+// });
+
+
+
+
+// lam moi
+
+// giao vien
+// Route::group(['prefix'=>'giao-vien'],function){
+//     Route::get('/don', function () {
+//         return view('user-teacher/don');
+
+//     });
+
+// });
+
+
+
+// guest
 Route::get('/', function () {
-    return view('user/home');
+    return view('user-guest.home');
 });
 
-Route::get('application', function(){
-	return view('user/don');
+Route::get('tin-tuc', function () {
+    return view('user-guest.tin-tuc');
+});
+Route::get('post', function () {
+    return view('user-guest.postview');
 });
 
-Route::get('post', function(){
-	return view('user/main');
+
+
+use App\cate_news;
+use App\News;
+
+Route::get('thu', function () {
+    $news = news::where('idNews' ,1);
+    dd($news->title);
 });
